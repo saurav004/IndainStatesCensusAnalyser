3bfad4f521d3de0ae82244ddd98a8901cc18e0bc
saurav004 <logtosaurav@gmail.com>
2020-12-02 20:50:55 +0530

    Added:CSVstate class and respective test cases

--- com/Bridgelabz/CensusAnalyser/CSVstates.py
+++ com/Bridgelabz/CensusAnalyser/CSVstates.py
@@ -1,28 +1,25 @@
 import pandas as pd
+
 from com.Bridgelabz.CensusAnalyser.CensusAnalysisErrors import CensusAnalyserError
 from com.Bridgelabz.CensusAnalyser.IndiaCensusCSV import IndiaCensusCSV
 
 
-class CSVLoader:
-
+class CSVStates:
     def __init__(self, path):
         self.path = path
 
-    def record_counter(self):
-        """
-        Count record in file
-        :return: number of records in file
-        """
+    def record_counter(self, ):
+        rows = []
         try:
             col_names = repr(IndiaCensusCSV()).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
             return len(data)
         except FileNotFoundError:
-            raise CensusAnalyserError("Check file path")
+            raise CensusAnalyserError("file path not available")
         except ValueError:
-            raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
+            raise CensusAnalyserError("Wrong Delimiter or Invalid Headers")
 
 
-if __name__ == "__main__":
-    CSV = CSVLoader(path="C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndiaStateCensusData.csv")
+if __name__ == '__main__':
+    CSV = CSVStates(path="C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndiaStateCensusData.csv")
     print(CSV.record_counter())
--- com/Bridgelabz/Census_Analyser_Testing/test_census_analyser.py
+++ com/Bridgelabz/Census_Analyser_Testing/test_census_analyser.py
@@ -1,3 +1,4 @@
+from com.Bridgelabz.CensusAnalyser.CSVstates import CSVStates
 from com.Bridgelabz.CensusAnalyser.CensusAnalysisErrors import CensusAnalyserError
 from com.Bridgelabz.CensusAnalyser.main import CSVLoader
 import pytest
@@ -5,7 +6,7 @@ import pytest
 CENSUS_CSV_FILE_PATH = "C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndiaStateCensusData.csv"
 CENSUS_CSV_FILE_WRONG_PATH = "IndiaStateCensusData.csv"
 CENSUS_CSV_FILE_WRONG_TYPE = "C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndiaStateCensusData.xls"
-CENSUS_CSV_FILE_WRONG_DELIMITER = "C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/USCensusData.xls"
+CENSUS_CSV_FILE_WRONG_DELIMITER = "C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndianStateCensusDifferentDelimiter.csv"
 CENSUS_CSV_WRONG_HEADER_FILE_PATH = "C:/Users/Saurabh/PycharmProjects/IndianStatesCensusAnalyser/data/IndiaStateCensusData1.csv"
 
 
@@ -45,3 +46,41 @@ def test_record_counter_for_wrong_header():
     csv_loader = CSVLoader(CENSUS_CSV_WRONG_HEADER_FILE_PATH)
     with pytest.raises(CensusAnalyserError):
         csv_loader.record_counter()
+
+
+def test_record_counter_csv_states():
+    """
+    check if length of records is same or not
+    """
+    csv_states_loader = CSVLoader(CENSUS_CSV_FILE_PATH)
+    assert csv_states_loader.record_counter() == 29
+
+
+def test_record_counter_for_wrong_file_path_csv_states():
+    """
+    Check if exception gets raised or not
+    """
+    csv_states_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_PATH)
+    with pytest.raises(CensusAnalyserError):
+        csv_states_loader.record_counter()
+
+
+def test_record_counter_for_wrong_file_type_csv_states():
+    """
+    Check if exception gets raised or not
+    """
+    csv_states_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_TYPE)
+    with pytest.raises(CensusAnalyserError):
+        csv_states_loader.record_counter()
+
+
+def test_record_counter_for_wrong_delimiter_csv_states():
+    csv_states_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_DELIMITER)
+    with pytest.raises(CensusAnalyserError):
+        csv_states_loader.record_counter()
+
+
+def test_record_counter_for_wrong_header_csv_states():
+    csv_states_loader = CSVStates(CENSUS_CSV_WRONG_HEADER_FILE_PATH)
+    with pytest.raises(CensusAnalyserError):
+        csv_states_loader.record_counter()
